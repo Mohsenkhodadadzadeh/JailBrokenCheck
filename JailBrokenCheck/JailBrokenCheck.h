e1c8fc5f501fbf3941737ecc740e63cb8a5b2bc7
black.roze2020 <860019071>
2017-04-20 16:10:14 +0430

    Initial Commit

---
+++ JailBrokenCheck/JailBrokenCheck.h
@@ -0,0 +1,19 @@
+//
+//  JailBrokenCheck.h
+//  JailBrokenCheck
+//
+//  Created by Admin on 4/20/17.
+//  Copyright © 2017 Mohsen khodadadzade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JailBrokenCheck.
+FOUNDATION_EXPORT double JailBrokenCheckVersionNumber;
+
+//! Project version string for JailBrokenCheck.
+FOUNDATION_EXPORT const unsigned char JailBrokenCheckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JailBrokenCheck/PublicHeader.h>
+
+
---
+++ JailBrokenCheckTests/JailBrokenCheckTests.m
@@ -0,0 +1,39 @@
+//
+//  JailBrokenCheckTests.m
+//  JailBrokenCheckTests
+//
+//  Created by Admin on 4/20/17.
+//  Copyright © 2017 Mohsen khodadadzade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JailBrokenCheckTests : XCTestCase
+
+@end
+
+@implementation JailBrokenCheckTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
